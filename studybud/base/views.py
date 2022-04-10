beccf1788b3dabfc80fa7232a7c1ceb2a9117007
Mark Bijl <mark_bijl@Hotmail.com>
2022-04-10 22:11:35 +0200

    Add search / filter option

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,6 +1,6 @@
 from multiprocessing import context
 from django.shortcuts import render, redirect
-
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -13,12 +13,17 @@ from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(Q(topic__name__icontains  = q) |
+    Q(name__icontains=q) |
+    Q(description__icontains=q)
+ )
 
     topics = Topic.objects.all()
+    room_count = rooms.count()
 
-    context = {"rooms": rooms, "topics": topics}
-    return render(request, "base/home.html", {"rooms": rooms})
+    context = {"rooms": rooms, "topics": topics, 'room_count': room_count}
+    return render(request, "base/home.html", context)
 
 
 def room(request, pk):
